7f5675edb876b7e833707918c21f8cb5e2c35c13
HongyuNie <nhy0613@outlook.com>
2022-11-06 14:12:32 -0800

    set up home page background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,12 @@
-// import React from "react";
-// import image from "../plant.jpg"
-// export default function Home() {
-//     return (
-//         <main>
-//             <img src={image} alt="Plant" className="absolute objecr-cover w-full h-full"/>
-//             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-//                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading -snug home-name">Hello. I'm Hongyu.</h1>
-//             </section>
-//         </main>
-//     )
-// }
-
-
-import React from "react"
-
+import React from "react";
+import image from "../leaf.jpg"
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="Plant" className="absolute objecr-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading -snug home-name">Hello. I'm Hongyu.</h1>
+            </section>
+        </main>
+    )
 }
